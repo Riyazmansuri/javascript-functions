8f2d4bd82d79439219f234eaedf103d76c35c8b5
Riyaz ul haq mansuri <riyazmansuri555@gmail.com>
2022-10-15 13:05:49 +0530

    getLivingNeighbors commit

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -51,6 +51,7 @@ const getNeighborsOf = ([x, y]) => [
 ];
 
 const getLivingNeighbors = (cell, state) => {
+  return getNeighborsOf(cell).filter(n => contains.bind(state)(n));
 };
 
 const willBeAlive = (cell, state) => {
